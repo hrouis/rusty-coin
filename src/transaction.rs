14839801f484cf92b7d187a1438cf0dfd92fbe94
hrouis <haythem.rouis@gmail.com>
2022-07-09 23:18:27 +0200

    [fix] build issues

--- src/transaction.rs
+++ src/transaction.rs
@@ -16,35 +16,39 @@ pub struct Transaction {
 
 impl Transaction {
     fn compute_inputs(&self) -> u64 {
-        &self.inputs.iter().map(|input| input.value).sum();
+        self.inputs.iter().map(|input| input.value).sum()
     }
 
     fn compute_outputs(&self) -> u64 {
-        &self.outputs.iter().map(|output| output.value).sum();
+        self.outputs.iter().map(|output| output.value).sum()
     }
 
     pub fn is_spendable(&self) -> bool {
-        &self.compute_inputs() > &self.compute_outputs();
+        self.compute_inputs() > self.compute_outputs()
+    }
+
+    pub fn is_coinbase(&self) -> bool {
+        self.inputs.len() == 0
     }
 }
 
 impl Hashable for TxOutput {
     fn bytes(&self) -> Vec<u8> {
         let mut to_bytes = vec![];
-        to_bytes.extend(&self.address.as_bytes());
-        to_bytes.extend(&self.value.to_le_bytes());
+        to_bytes.extend(self.address.as_bytes());
+        to_bytes.extend(self.value.to_le_bytes());
         to_bytes
     }
 }
 
 impl Hashable for Transaction {
     fn bytes(&self) -> Vec<u8> {
-        let mut tx_output = !vec![];
-        tx_output.extend(&self.inputs
+        let mut tx_output = vec![];
+        tx_output.extend(self.inputs
             .iter()
             .flat_map(|input| input.bytes())
             .collect::<Vec<u8>>());
-        tx_output.extend(&self.outputs
+        tx_output.extend(self.outputs
             .iter()
             .flat_map(|output| output.bytes())
             .collect::<Vec<u8>>());
