c1de876784f78dec5418ad24bc8dc8eb8c853920
hrouis <haythem.rouis@gmail.com>
2022-08-15 14:33:26 +0200

    [update] storage methods

--- database/src/storage.rs
+++ database/src/storage.rs
@@ -1,7 +1,9 @@
 use leveldb::database::Database;
 use leveldb::error::Error;
 use leveldb::kv::KV;
-use leveldb::options::{Options, WriteOptions};
+use leveldb::options::{Options, ReadOptions, WriteOptions};
+use serde::de::DeserializeOwned;
+use serde::Serialize;
 use tempdir::TempDir;
 
 pub struct DataStorage {
@@ -16,7 +18,7 @@ impl DataStorage {
         let db_path = tempdir.path();
         let mut options = Options::new();
         options.create_if_missing = true;
-        let mut database = Database::open(db_path, options).unwrap();
+        let database = Database::open(db_path, options).unwrap();
         DataStorage {
             name,
             path,
@@ -24,7 +26,29 @@ impl DataStorage {
         }
     }
 
-    pub fn save(&self, options: WriteOptions, key: i32, bytes: &[u8]) -> Result<(), Error> {
-        self.database.put(options, key, bytes)
+    pub fn save<T>(&self, options: WriteOptions, key: i32, data: &T) -> Result<(), Error>
+    where
+        T: Serialize + DeserializeOwned,
+    {
+        let bytes = serde_json::to_string(data).unwrap();
+        self.database.put(options, key, bytes.as_bytes())
+    }
+
+    pub fn read<T>(&self, options: ReadOptions<i32>, key: i32) -> Result<T, Error>
+    where
+        T: Serialize + DeserializeOwned,
+    {
+        let response = self.database.get(options, key);
+        return match response {
+            Ok(data) => {
+                let data_vec = data.unwrap();
+                Result::Ok(serde_json::from_slice(data_vec.as_slice()).unwrap())
+            }
+            Err(e) => Result::Err(e),
+        };
+    }
+
+    pub fn delete(&self, options: WriteOptions, key: i32) -> Result<(), Error> {
+        self.database.delete(options, key)
     }
 }
